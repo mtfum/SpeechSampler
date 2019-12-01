0c1c9d9a51dceb21774888be3789b27f25d369eb
mtfum <f.yamanaka1127@gmail.com>
2019-12-02 04:21:20 +0900

    Initial Commit

---
+++ SpeechSampler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpeechSampler
+//
+//  Created by Fumiya Yamanaka on 2019/12/02.
+//  Copyright © 2019 mtfum. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
